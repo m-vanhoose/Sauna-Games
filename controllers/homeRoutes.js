103a4237d950d390536744df720dd4f124f53986
m-vanhoose <m-vanhoose@live.com>
2023-05-01 19:59:41 -0400

    fixed login route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,28 +3,7 @@ const { Game, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-  try {
-    // Get all projects and JOIN with user data
-    const gameData = await Game.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    // Serialize data so the template can read it
-    const game = GameData.map((game) => game.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      projects, 
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  res.render("login")
 });
 
 router.get('/game/:id', async (req, res) => {
