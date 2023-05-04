a21abd514fea32267f0928232e8262688232acf9
aziz <mraziz.aden@gmail.com>
2023-05-04 17:58:45 -0400

    passport library auth

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const passport = require('passport')
+const GoogleStragety = require( 'passport-google-oauth2' ).Stragety;
 const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
@@ -14,6 +16,9 @@ const PORT = process.env.PORT || 3001;
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
+function isLoggedIn(req,res,next){
+  req.user ? next() : res.sendStatus(401);
+}
 const sess = {
   secret: 'Super secret secret',
   cookie: {
@@ -31,6 +36,9 @@ const sess = {
 
 app.use(session(sess));
 
+app.get(passport.initialize ());
+app.use(passport.session());
+
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -39,6 +47,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.post('/login/password',
+  passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }),
+  function(req, res) {
+    res.redirect('/~' + req.user.username);
+  });
+
+  app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+  app.use('/auth/logout', (req,res) => {
+    req.session.destroy();
+    res.send("See you again!!");
+  })
+
+  
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
