1d480f251ebc502547f3e7d664aa9f135f1a9ce7
OMR K9 <121472588+oreynosa12@users.noreply.github.com>
2023-05-06 20:56:12 -0400

    Removed code from homeRoutes, Catalog and Main

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,9 +6,9 @@ router.get('/', async (req, res) => {
   res.render("login")
 });
 
-router.get('/homepage', async (req, res) => {
-  res.render("catalog")
-});
+// router.get('/homepage', async (req, res) => {
+//   res.render("catalog")
+// });
 
 
 router.get('/catalog', async (req, res) => {
