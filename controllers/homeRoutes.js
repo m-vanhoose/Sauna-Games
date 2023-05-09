b5044bc15ae29874aeb93f9bda5698eef0d404d6
Andrew Jester <andrewj5787@jesterz.net>
2023-05-09 14:40:12 -0400

    cleaned up some code

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -77,28 +77,6 @@ router.post('/library', withAuth, async (req, res) => {
   }
 });
 
-// router.get('/game/:id', async (req, res) => {
-//   try {
-//     const gameData = await Game.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['name'],
-//         },
-//       ],
-//     });
-
-//     const game = gameData.get({ plain: true });
-
-//     res.render('game', {
-//       ...game,
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
 router.get('/game/:id', async (req, res) => {
   const gameData = await Game.findByPk(req.params.id);
   const game = gameData.get({ plain: true });
@@ -106,27 +84,6 @@ router.get('/game/:id', async (req, res) => {
     game
   })
 })
-
-
-// Use withAuth middleware to prevent access to route
-// router.get('/profile', withAuth, async (req, res) => {
-//   try {
-//     // Find the logged in user based on the session ID
-//     const userData = await User.findByPk(req.session.user_id, {
-//       attributes: { exclude: ['password'] },
-//       include: [{ model: Game }],
-//     });
-
-//     const user = userData.get({ plain: true });
-
-//     res.render('profile', {
-//       ...user,
-//       logged_in: true
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
